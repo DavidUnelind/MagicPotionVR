905afae068da9b25248affee5512091ab7b923cc
LindaBlom <linda.johanna.blom@gmail.com>
2026-03-28 02:06:07 +0100

    pouring animation

---
+++ Assets/liquid_feature/Scripts/Stream.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Stream : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particles;    
+    private LineRenderer lineRenderer = null;
+
+    private Vector3 targetPosition = Vector3.zero;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.widthMultiplier = 1.0f;
+    }
+
+
+    private void Start()
+    {
+        MoveToPosition(0, transform.position);
+        MoveToPosition(1, transform.position);
+        
+        
+    }
+
+    public void Begin()
+    {
+        StartCoroutine(BeginPour());
+    }
+
+    private IEnumerator BeginPour()
+    {
+        while(gameObject.activeSelf)
+        {
+            targetPosition = FindEndPoint();
+            MoveToPosition(0, transform.position);
+            MoveToPosition(1, targetPosition);
+            particles.transform.position = targetPosition;
+            
+
+            yield return null;    
+        }
+        
+    }
+
+    // shoot a ray downwards to fins endpoint
+    private Vector3 FindEndPoint()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        Physics.Raycast(ray, out hit, 10.0f);
+        Vector3 endPoint = hit.collider ? hit.point : ray.GetPoint(10.0f);
+//
+        return endPoint;
+        //
+    }
+
+    private void MoveToPosition(int index, Vector3 targetPosition)
+    {
+        lineRenderer.SetPosition(index, targetPosition);
+        
+    }
+}
---
+++ Assets/liquid_feature/Scripts/Tilt_detection.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class Tilt_detection : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform bottle;
+    [SerializeField] private Transform pourPoint;
+    [SerializeField] private GameObject streamObject;
+
+
+
+    // Detta gör så att variabler syns i inspektorn
+    [Header("Tilt Settings")]
+    [SerializeField] private float pourThreshold = 0;
+
+    
+
+    private bool isPouring;
+    private Stream currentStream = null;
+    
+
+    private void Start()
+    {
+        if (bottle == null)
+            bottle = transform;
+        streamObject = Instantiate(streamObject,pourPoint.position, Quaternion.identity, transform );
+        currentStream = streamObject.GetComponent<Stream>();
+        streamObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // 1 = helt upprätt, 0 = ungefär 90 graders tilt, < 0 = upp och ner
+        float uprightDot = Vector3.Dot(bottle.up, Vector3.up);
+        bool shouldPour = uprightDot < pourThreshold;
+
+
+        //Debug.Log($"uprightDot: {uprightDot}, shouldPour: {shouldPour}");
+   
+
+        if (shouldPour != isPouring)
+        {
+            isPouring = shouldPour;
+        }
+
+        if(isPouring )
+        {
+            //Debug.Log($"Current Stream: {currentStream}");
+            StartPour();
+        }
+        else
+        {
+            EndPour();
+        }
+    }
+
+
+    private void StartPour()
+    {
+        Debug.Log("POURING STARTED");
+        streamObject.SetActive(true);
+        currentStream.Begin();
+    }
+
+    private void EndPour()
+    {
+        Debug.Log("POURING STOPPED");
+        //streamObject = null;
+        streamObject.SetActive(false);
+        
+    }
+
+
+   
+}
